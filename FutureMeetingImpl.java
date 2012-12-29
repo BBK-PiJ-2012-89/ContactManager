6d3ea229bd6828cf03b627e600ad2051f7003a16
Charlie Brodie <charliebrodie@Charlie-Brodies-MacBook-Pro.local>
2012-12-29 13:49:05 +0000

    These two are just the simplest possible conversions of their interfaces with no functionality whatsoever yet.

---
+++ FutureMeetingImpl.java
@@ -0,0 +1,7 @@
+/**
+* A meeting to be held in the future
+*/
+public class FutureMeetingImpl implements FutureMeeting{
+	// No methods here, this is just a naming interface
+	//(i.e. only necessary for type checking and/or downcasting)
+}
