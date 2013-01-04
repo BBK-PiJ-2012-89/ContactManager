c6b0717a508976f19e91a8533b9076108bc2388c
Charlie Brodie <charliebrodie@Charlie-Brodies-MacBook-Pro.local>
2013-01-04 16:26:21 +0000

    This is the class which contains the main method, I am also using it to store all the code for the user interface, so far I have successfully implemented the add Contact section which I am very pleased with.

---
+++ ContactStarter.java
@@ -0,0 +1,95 @@
+public class ContactStarter{
+
+    public static void main(String[] args) {
+        ContactStarter launcher = new ContactStarter();
+        launcher.launch();
+    }
+
+    public void launch() {
+
+    	welcome();
+
+    }
+
+	public void welcome(){
+
+		int selection = 0;
+		
+
+		System.out.println("Welcome to the new and improved Contact Manager Pro!");
+		System.out.println("What would you like to do?");
+		System.out.println("");
+		System.out.println("Enter 1 to add a new contact");
+		System.out.println("Enter 2 to add a new meeting");
+		System.out.println("Enter 3 to access a past meeting");
+		System.out.println("Enter 4 to add meeting notes");
+		System.out.println("Enter 5 to access a future meeting");
+		System.out.println("");
+
+		while(selection == 0){
+
+			String str = System.console().readLine();
+
+			try{
+				selection = Integer.parseInt(str);
+
+			} catch (NumberFormatException ex){
+				System.out.println("That was not an integer!");
+				ex.printStackTrace();
+			}
+		}
+
+		mainMenu(selection);
+	}
+
+	public void mainMenu(int a){
+		ContactManagerImpl newContactManager = new ContactManagerImpl();
+
+		if(a == 1){
+			System.out.print("You have selected add new Contact, begin by entering the contacts name or type EXIT to return to the main menu: ");
+			String name = System.console().readLine();
+			if(name.equals("EXIT")){
+				welcome();
+			}
+			System.out.print("Now please enter any notes that you may have regarding the contact: ");
+			String notes = System.console().readLine();
+
+			newContactManager.addNewContact(name, notes);
+
+			System.out.println("Thank you, your new contact has now been added to the database, you will now be returned to the main menu.");
+			welcome();
+		}
+
+
+		if(a == 2){
+			System.out.println("You have selected add new meeting, begin by entering the contacts name or type EXIT to return to the main menu: ");
+			String str = System.console().readLine();
+			if(str.equals("EXIT")){
+				welcome();
+			}
+		}
+		if(a == 3){
+			System.out.println("You have selected access past meeting, begin by entering the contacts name or type EXIT to return to the main menu: ");
+			String str = System.console().readLine();
+			if(str.equals("EXIT")){
+				welcome();
+			}
+		}
+		if(a == 4){
+			System.out.println("You have selected add meeting notes, begin by entering the contacts name or type EXIT to return to the main menu: ");
+			String str = System.console().readLine();
+			if(str.equals("EXIT")){
+				welcome();
+			}
+		}
+		if(a == 5){
+			System.out.println("You have selected access Future Meeting, begin by entering the contacts name or type EXIT to return to the main menu: ");
+			String str = System.console().readLine();
+			if(str.equals("EXIT")){
+				welcome();
+			}
+		}
+
+	}
+
+}
